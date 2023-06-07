148ebe3c73d535c5671717ed61e6bf7432a25330
a8787753 <56746354+a8787753@users.noreply.github.com>
2023-06-07 18:43:51 +0800

    Changes

--- test_code/test_seq2seq_torch.py
+++ test_code/test_seq2seq_torch.py
@@ -1,6 +1,6 @@
 import torch
-import torch.nn
-import torch.optim
+import torch.nn as nn
+import torch.optim as optim
 
 from torchtext.legacy.datasets import Multi30k
 from torchtext.legacy.data import Field, BucketIterator
@@ -68,4 +68,35 @@ train_iterator, valid_iterator, test_iterator = BucketIterator.splits(
     batch_size=BATCH_SIZE,
     device=device
 )
+
+
+class Encoder(nn.Module):
+    def __init__(self, input_dim, emb_dim, hid_dim, n_layers, dropout):
+        super().__init__()
+
+        self.hid_dim = hid_dim
+        self.n_layers = n_layers
+
+        self.embedding = nn.Embedding(input_dim, emb_dim)
+
+        self.rnn = nn.LSTM(emb_dim, hid_dim, n_layers, dropout=dropout)
+
+    def forward(self, src):
+
+        # src = [src len, batch size]
+
+        embedded = self.dropout(self.embedding(src))
+
+        # embedded = [src len, batch size, emb dim]
+
+        outputs, (hidden, cell) = self.rnn(embedded)
+
+        # outputs = [src len, batch size, hid dim * n directions]
+        # hidden = [n layers * n directions, batch size, hid dim]
+        # cell = [n layers * n directions, batch size, hid dim]
+
+        # outputs are always from the top hidden layer
+
+        return hidden, cell
+
 
