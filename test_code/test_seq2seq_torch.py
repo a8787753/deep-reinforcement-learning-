b926f9cb89c7ff1ad3326f0587780f6ee7fc8c69
a8787753 <56746354+a8787753@users.noreply.github.com>
2023-06-07 23:27:30 +0800

    1

--- test_code/test_seq2seq_torch.py
+++ test_code/test_seq2seq_torch.py
@@ -81,6 +81,8 @@ class Encoder(nn.Module):
 
         self.rnn = nn.LSTM(emb_dim, hid_dim, n_layers, dropout=dropout)
 
+        self.dropout = nn.Dropout(dropout)
+
     def forward(self, src):
 
         # src = [src len, batch size]
@@ -209,7 +211,7 @@ OUTPUT_DIM = len(TRG.vocab)
 ENC_EMB_DIM = 256
 DEC_EMB_DIM = 256
 HID_DIM = 512
-N_LAYERS = 2
+N_LAYERS = 4
 ENC_DROPOUT = 0.5
 DEC_DROPOUT = 0.5
 
@@ -265,3 +267,87 @@ def train(model, iterator, optimizer, criterion, clip):
 
         # trg = [(trg len - 1) * batch size]
         # output = [(trg len - 1) * batch size, output_dim]
+
+        loss = criterion(output, trg)
+
+        loss.backward()
+
+        torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
+
+        optimizer.step()
+
+        epoch_loss += loss.item()
+
+    return epoch_loss / len(iterator)
+
+
+def evaluate(model, iterator, criterion):
+
+    model.eval()
+
+    epoch_loss = 0
+
+    with torch.no_grad():
+
+        for i, batch in enumerate(iterator):
+
+            src = batch.src
+            trg = batch.trg
+
+            output = model(src, trg, 0)  # turn off teacher forcing
+
+            # trg = [trg len, batch size]
+            # output = [trg len, batch size, output dim]
+
+            output_dim = output.shape[-1]
+
+            output = output[1:].view(-1, output_dim)
+            trg = trg[1:].view(-1)
+
+            # trg = [(trg len - 1) * batch size]
+            # output = [(trg len - 1) * batch size, output_dim]
+
+            loss = criterion(output, trg)
+
+            epoch_loss += loss.item()
+
+    return epoch_loss / len(iterator)
+
+
+def epoch_time(start_time, end_time):
+    elapsed_time = end_time - start_time
+    elapsed_mins = int(elapsed_time / 60)
+    elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
+    return elapsed_mins, elapsed_secs
+
+
+N_EPOCHS = 10
+CLIP = 1
+
+best_valid_loss = float('inf')
+
+for epoch in range(N_EPOCHS):
+
+    start_time = time.time()
+
+    train_loss = train(model, train_iterator, optimizer, criterion, CLIP)
+    valid_loss = evaluate(model, valid_iterator, criterion)
+
+    end_time = time.time()
+
+    epoch_mins, epoch_secs = epoch_time(start_time, end_time)
+
+    if valid_loss < best_valid_loss:
+        best_valid_loss = valid_loss
+        torch.save(model.state_dict(), 'tutl_model.pt')
+
+    print(f'Epoch: {epoch+1:02} | Time: {epoch_mins}m {epoch_secs}s')
+    print(f'\tTrain Loss: {train_loss:.3f} | Train PPL: {math.exp(train_loss):7.3f}')
+    print(f'\tVal. Loss: {valid_loss:.3f} | Val PPL: {math.exp(valid_loss):7.3f}')
+
+
+model.load_state_dict(torch.load('tutl_model.pt'))
+
+test_loss = evaluate(model, test_iterator, criterion)
+
+print(f'| Test Loss: {test_loss:.3f} | Test PPL: {math.exp(test_loss):7.3f} |')
