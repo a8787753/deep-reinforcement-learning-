b46ffa437b7fd0a96e6bee576fc8b5fd28af19c6
a8787753 <56746354+a8787753@users.noreply.github.com>
2023-06-07 22:51:50 +0800

    Changes

--- test_code/test_seq2seq_torch.py
+++ test_code/test_seq2seq_torch.py
@@ -233,4 +233,35 @@ def count_parameters(model):
 
 print(f'The model has {count_parameters(model):,} trainable parameters')
 
+optimizer = optim.Adam(model.parameters())
 
+TRG_PAD_IDX = TRG.vocab.stoi[TRG.pad_token]
+
+criterion = nn.CrossEntropyLoss(ignore_index=TRG_PAD_IDX)
+
+
+def train(model, iterator, optimizer, criterion, clip):
+
+    model.train()
+
+    epoch_loss = 0
+
+    for i, batch in enumerate(iterator):
+
+        src = batch.src
+        trg = batch.trg
+
+        optimizer.zero_grad()
+
+        output = model(src, trg)
+
+        # trg = [trg len, batch size]
+        # output = [trg len, batch size, output dim]
+
+        output_dim = output.shape[-1]
+
+        output = output[1:].view(-1, output_dim)
+        trg = trg[1:].view(-1)
+
+        # trg = [(trg len - 1) * batch size]
+        # output = [(trg len - 1) * batch size, output_dim]
