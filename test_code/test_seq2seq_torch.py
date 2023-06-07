6750dec84d685f1cf383ba185aec1c6624b68774
a8787753 <56746354+a8787753@users.noreply.github.com>
2023-06-07 09:46:53 +0800

    a

--- test_code/test_seq2seq_torch.py
+++ test_code/test_seq2seq_torch.py
@@ -1,10 +1,26 @@
 import torch
 import torch.nn
-import tirch.optim
+import torch.optim
 
+from torchtext.legacy.datasets import Multi30k
+from torchtext.legacy.data import Field, BucketIterator
 
+import spacy
+import numpy as np
 
+import random
+import math
+import time
 
+# print(torch.__version__)
+# print(torch.cuda.is_available())
 
-print(torch.torch_version.__version__)
-print(torch.cuda.is_available())
+SEED = 1234
+random.seed(SEED)
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+torch.cuda.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
+
+spacy_de = spacy.load('de_core_news_sm')
+spacy_en = spacy.load('en_core_web_sm')
