da16bfecdede7ad6f181ace849ad8930d3d9a5e3
a8787753 <56746354+a8787753@users.noreply.github.com>
2023-06-10 08:12:47 +0800

    Changes

--- test_code/test_transformer.py
+++ test_code/test_transformer.py
@@ -121,3 +121,18 @@ class PositionwiseFeedForward(nn.Module):
 
     def forward(self, x):
         return self.w2(self.dropout(F.relu(self.w1(x))))
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, features, eps=1e-6):
+        super(LayerNorm, self).__init__()
+
+        self.a2 = nn.Parameter(torch.ones(features))
+        self.b2 = nn.Parameter(torch.zeros(features))
+
+        self.eps = eps
+
+    def forward(self, x):
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(-1, keepdim=True)
+        return self.a2 * (x - mean) / (std + self.eps) + self.b2
