bdaa75fd3b87e3d578f31e5d11ea7a3b8fabf72b
a8787753 <56746354+a8787753@users.noreply.github.com>
2023-06-10 10:50:20 +0800

    Changes

--- test_code/test_transformer.py
+++ test_code/test_transformer.py
@@ -166,3 +166,17 @@ class EncoderLayer(nn.Module):
         x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, mask))
         return self.sublayer[1](x, self.feed_forward)
 
+
+class Encoder(nn.Module):
+    def __init__(self, layer, N):
+        super(Encoder, self).__init__()
+
+        self.layers = clones(layer, N)
+        self.norm = LayerNorm(layer.size)
+
+    def forward(self, x, mask):
+        for layer in self.layers:
+            x = layer(x, mask)
+        return self.norm(x)
+
+
