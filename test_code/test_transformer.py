aed068fad6cbc1ebcc6431072efab2ef395537f3
a8787753 <56746354+a8787753@users.noreply.github.com>
2023-06-10 12:56:33 +0800

    Changes

--- test_code/test_transformer.py
+++ test_code/test_transformer.py
@@ -225,3 +225,24 @@ class Generator(nn.Module):
         return F.log_softmax(self.project(x), dim=-1)
 
 
+class EncoderDecoder(nn.Module):
+    def __init__(self, encoder, decoder, source_embed, target_embed, generator):
+        super(EncoderDecoder, self).__init__()
+
+        self.encoder = encoder
+        self.decoder = decoder
+        self.src_embed = source_embed
+        self.tgt_embed = target_embed
+        self.generator = generator
+
+    def forward(self, source, target, source_mask, target_mask):
+
+        return self.decode(self.encode(source, source_mask), source_mask, target, target_mask)
+
+    def encode(self, source, source_mask):
+        return self.encoder(self.src_embed(source), source_mask)
+
+    def decode(self, memory, source_mask, target, target_mask):
+        return self.decoder(self.tgt_embed(target), memory, source_mask, target_mask)
+
+
