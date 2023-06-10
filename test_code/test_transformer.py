c0b07804b73d59f06eb9c960df059274db91ca47
a8787753 <56746354+a8787753@users.noreply.github.com>
2023-06-10 13:09:32 +0800

    Changes

--- test_code/test_transformer.py
+++ test_code/test_transformer.py
@@ -246,3 +246,27 @@ class EncoderDecoder(nn.Module):
         return self.decoder(self.tgt_embed(target), memory, source_mask, target_mask)
 
 
+def make_model(source_vocab, target_vocab, N=6, d_model=512, d_ff=2048, head=8, dropout=0.1):
+    c = copy.deepcopy
+
+    attn = MultiHeadAttention(head, d_model)
+
+    ff = PositionwiseFeedForward(d_model, d_ff, dropout)
+
+    position = PositionalEncoding(d_model, dropout)
+
+    model = EncoderDecoder(
+        Encoder(EncoderLayer(d_model, c(attn), c(ff), dropout), N),
+        Decoder(DecoderLayer(d_model, c(attn), c(attn), c(ff), dropout), N),
+        nn.Sequential(Embeddings(d_model, source_vocab), c(position)),
+        nn.Sequential(Embeddings(d_model, target_vocab), c(position)),
+        Generator(d_model, target_vocab)
+    )
+
+    for p in model.parameters():
+        if p.dim() > 1:
+            nn.init.xavier_uniform(p)
+
+    return model
+
+
