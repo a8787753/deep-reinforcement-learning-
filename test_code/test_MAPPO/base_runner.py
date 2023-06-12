718cb2026dae8054d5d24406bbcb3d151cf51fd1
a8787753 <56746354+a8787753@users.noreply.github.com>
2023-06-12 13:34:11 +0800

    Changes

---
+++ test_code/test_MAPPO/base_runner.py
@@ -0,0 +1,58 @@
+import wandb
+import os
+import numpy as np
+import torch
+# from tensorboardX import SummaryWriter
+# from onpolicy.utils.shared_buffer import SharedReplayBuffer
+
+
+def _t2n(x):
+    '''Convert torch tensor to a numpy array.'''
+    return x.detach().cpu().numpy()
+
+
+class Runner(object):
+    '''
+    Base class for training recurrent policies.
+    :param config: (dict) Config dictionary containing parameters for training.
+    '''
+
+    def __init__(self, config):
+
+        self.all_args = config['all_args']
+        self.envs = config['envs']
+        self.eval_envs = config['eval_envs']
+        self.device = config['divice']
+        self.num_agents = config['num_agents']
+        if config.__contains__('render_envs'):
+            self.render_envs = config['render_envs']
+
+        # parameters
+        self.env_name = self.all_args.env_name
+        self.algorithm_name = self.all_args.algorithm_name
+        self.experiment_name = self.all_args.experiment_name
+        self.use_centralized_V = self.all_args.use_centralized_V
+        self.use_obs_instead_of_state = self.all_args.use_obs_instead_of_state
+        self.num_env_steps = self.all_args.num_env_steps
+        self.episode_length = self.all_args.episode_length
+        self.n_rollout_threads = self.all_args.n_rollout_threads
+        self.n_eval_rollout_threads = self.all_args.n_eval_rollout_threads
+        self.n_render_rollout_threads = self.all_args.n_render_rollout_threads
+        self.use_linear_lr_decay = self.all_args.use_linear_lr_decay
+        self.hidden_size = self.all_args.hidden_size
+        self.use_wandb = self.all_args.use_wandb
+        self.use_render = self.all_args.use_render
+        self.recurrent_N = self.all_args.recurrent_N
+
+        # interval
+        self.save_interval = self.all_args.save_interval
+        self.use_eval = self.all_args.use_eval
+        self.eval_interval = self.all_args.eval_interval
+        self.log_interval = self.all_args.log_interval
+
+        # dir
+        self.model_dir = self.all_args.model_dir
+
+        if self.use_wandb:
+            self.save_dir = str(wandb.run.dir)
+            self.run_dir = str(wandb.run.dir)
